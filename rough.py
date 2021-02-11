977e9493c9d1ec726dc46c9b0c21cadb2c607b26
ABHISHEK SINGH RAWAT <abhishek.rawat1410@gmail.com>
2021-02-11 19:03:48 +0530

    changes

--- rough.py
+++ rough.py
@@ -1,8 +1,1 @@
-grocery = ["harpic", "vim bar", "deodrant", "bhindi", 56]
-print(grocery[4])
-numbers = [1, 6, 2, 3, 4]
-numbers.sort()
-numbers.reverse()
-there are "<it incluse it also"
-it consist of the is the call by reference is called of the value of the data is yht < "it incluse" >
-print(numbers)
+print("hello")
